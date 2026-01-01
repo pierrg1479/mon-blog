90833704a9b5985bc96c680906c2a46e4bf4971d
pierrg1479 <pierre.gincheleau@gmail.com>
2026-01-01 22:26:01 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://the-optimization-guy.vercel.app',
